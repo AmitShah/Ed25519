5b681178f6e75f46fdaaf15f7801668530050615
Amit Shah <amitshah0t7@gmail.com>
2024-03-22 00:30:51 -0500

    add transfer with authorization to erc20

--- contracts/HashCastKarma.sol
+++ contracts/HashCastKarma.sol
@@ -8,6 +8,10 @@ import "./libraries/Ed25519.sol";
 import "hardhat/console.sol";
 
 contract Karma is ERC20Base{
+    //EIP-3009
+    bytes32 private immutable _TRANSFER_WITH_AUTHORIZATION_TYPEHASH = 
+      keccak256("TransferWithAuthorization(bytes32 from,address to,uint256 value,uint256 nonce,uint256 deadline)");
+      
      bytes32 private immutable _PERMIT_EDDSA_TYPEHASH =
         keccak256("Permit(bytes32 pubkey,address spender,uint256 value,uint256 nonce,uint256 deadline)");
 
@@ -23,13 +27,28 @@ contract Karma is ERC20Base{
         bytes32 s
     ) public  {
         address vaddress = Ed25519.getVirtualAddress(pubkey);
-        require(block.timestamp <= deadline, "ERC20PermitEDDSA: expired deadline");
+        require(block.timestamp <= deadline, "permitEDDSA: expired deadline");
         bytes32 structHash = keccak256(abi.encode(_PERMIT_EDDSA_TYPEHASH, pubkey, spender, value, _useNonce(vaddress), deadline));
         bytes32 digest = ECDSA.toTypedDataHash(DOMAIN_SEPARATOR(), structHash);
         bool valid = Ed25519.verify(pubkey, r, s, abi.encodePacked(digest));       
-        require(valid, "ERC20PermitEDDSA: invalid signature");
+        require(valid, "permitEDDSA: invalid signature");
         _approve(vaddress, spender, value);
     }
 
-
+    function transferWithAuthorization(
+        bytes32 pubkey,
+        address to,
+        uint256 value,
+        uint256 deadline,
+        bytes32 r,
+        bytes32 s
+    ) external{
+        address vaddress = Ed25519.getVirtualAddress(pubkey);
+        require(block.timestamp <= deadline, "transferWithAuthorization: expired deadline");
+        bytes32 structHash = keccak256(abi.encode(_TRANSFER_WITH_AUTHORIZATION_TYPEHASH, pubkey, to, value, _useNonce(vaddress), deadline));
+        bytes32 digest = ECDSA.toTypedDataHash(DOMAIN_SEPARATOR(), structHash);
+        bool valid = Ed25519.verify(pubkey, r, s, abi.encodePacked(digest));       
+        require(valid, "transferWithAuthorization: invalid signature");
+        _transfer(vaddress, to, value);        
+    }
 }
